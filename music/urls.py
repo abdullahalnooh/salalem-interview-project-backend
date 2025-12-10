4212e1bdb7d94c408c3550f5b750eb83633d584d
abdullahalnooh <abodi.o.alnooh@gmail.com>
2025-12-10 20:06:31 +0300

    setting up the super admin and disabling CSRF protection

--- music/urls.py
+++ music/urls.py
@@ -2,8 +2,9 @@ from django.urls import include, path
 import music.views as view
 from .schema import schema
 from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('', view.index, name='index'),
-    path('graphql/', GraphQLView.as_view(graphiql=True , schema=schema), name='graphql'),
+    path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True , schema=schema))),
 ]
