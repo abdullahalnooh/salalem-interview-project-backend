7adb14da5ad1b880a62e8481dc19f198edffad00
abdullahalnooh <abodi.o.alnooh@gmail.com>
2025-12-10 14:45:40 +0300

    wrote the models

--- music/models.py
+++ music/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class artist(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Album(models.Model):
+    artist = models.ForeignKey(artist, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    def __str__(self):
+        return f"{self.artist} {self.name} {self.release_date}"
+
+class Song(models.Model):
+    artist = models.ForeignKey(artist, on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    def __str__(self):
+        return f"{self.artist} {self.album} {self.title}"
