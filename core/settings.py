409ff001803baec594ff025639ec2863aa9df969
abdullahalnooh <abodi.o.alnooh@gmail.com>
2025-12-13 21:57:33 +0300

    creating dockerfile

--- core/settings.py
+++ core/settings.py
@@ -25,8 +25,15 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "46.224.71.154",  
+]
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://46.224.71.154:8080",
+]
 
 # Application definition
 
